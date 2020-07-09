9923670980051044b3f42fd2f16f363109c3a9a1
Zebediah Perkins <zebmperkins@gmail.com>
2020-07-09 19:23:24 -0400

    add test for equals function

--- test/util.test.js
+++ test/util.test.js
@@ -1,39 +1,37 @@
-const util = require('../src/util');
 const assert = require('assert');
-const { findLowestPrecedence, buildNodePointers, buildNode } = require('../src/util');
-const BinaryNode = require('../src/BinaryNode');
+const { equals } = require('../src/util');
 
-describe('#findLowestPrecedence', () => {
-    it('should find the lowest precedence operator', () => {
-        assert.strictEqual(findLowestPrecedence('4*5+1-4/12').lowestPrecedenceOperator, '-');
-        assert.strictEqual(findLowestPrecedence('4*5+1').lowestPrecedenceOperator, '+');
-        assert.strictEqual(findLowestPrecedence('4*5').lowestPrecedenceOperator, '*');
-        assert.strictEqual(findLowestPrecedence('4').lowestPrecedenceOperator, null);
+describe('#equals', () => {
+    it('should return that the values are equal', () => {
+        assert.strictEqual(equals(4, 4), true);
     });
-});
-
-describe('#buildNodePointers', () => {
-    it('should reconfigure a binary node\'s null pointers', () => {
-        let node1 = buildNodePointers(new BinaryNode(null, null, '+'), ['3', '+', '5']);
-        assert.strictEqual(node1.data, '+');
-        assert.strictEqual(node1.left.data, '3');
-        assert.strictEqual(node1.right.data, '5');
-        let node2 = buildNodePointers(new BinaryNode(null, null, '*'), ['9', '^', '8', '/', '3', '*', '5']);
-        assert.strictEqual(node2.data, '*');
-        assert.strictEqual(node2.left.data, '/');
-        assert.strictEqual(node2.right.data, '5');
-        assert.strictEqual(node2.left.left.data, '^');
-        assert.strictEqual(node2.left.right.data, '3');
-        assert.strictEqual(node2.left.left.left.data, '9');
-        assert.strictEqual(node2.left.left.right.data, '8');
+    it('should return that the values are not equal', () => {
+        assert.strictEqual(equals(5, 1), false);
     });
-});
-
-describe('#buildNode', () => {
-    it('should build a node with data equal to \'5\' and null ptrs', () => {
-        let node = buildNode(['5']);
-        assert.strictEqual(node.data, '5');
-        assert.strictEqual(node.left, null);
-        assert.strictEqual(node.right, null);
+    class TestClass {
+        constructor(val1, val2) {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+        isEqual(obj) {
+            if (obj.val1 && obj.val2) return obj.val1 === this.val1 && obj.val2 === this.val2;
+            else if (!this.val1 && !this.val2) return true;
+            return false;
+        }
+    }
+    it('should return that the objects are equal', () => {
+        let obj1 = new TestClass("testing", 123);
+        let obj2 = obj1;
+        assert.strictEqual(equals(obj1, obj2), true);
+    });
+    it('should return that the objects are equal', () => {
+        let obj1 = new TestClass("testing", 123);
+        let obj2 = new TestClass("testing", 123);
+        assert.strictEqual(equals(obj1, obj2), true);
+    });
+    it('should return that the objects are not equal', () => {
+        let obj1 = new TestClass("testing", 123);
+        let obj2 = new TestClass("Testing", 123);
+        assert.strictEqual(equals(obj1, obj2), false);
     });
 });
