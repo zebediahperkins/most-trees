bf6343332f0bdf1faba0a6424379328b93c1e7c4
Zebediah Perkins <zebmperkins@gmail.com>
2020-07-09 19:22:52 -0400

    add tests for new methods

--- test/ExpressionTree.test.js
+++ test/ExpressionTree.test.js
@@ -1,4 +1,5 @@
 const ExpressionTree = require('../src/ExpressionTree');
+const BinaryNode = require('../src/BinaryNode');
 const assert = require('assert');
 
 describe('ExpressionTree', () => {
@@ -54,6 +55,44 @@ describe('ExpressionTree', () => {
         });
         it('should return the simplified value of the tree with variables', () => {
             assert.strictEqual(new ExpressionTree('6+7-4*x').solveTree(), '13-4*x');
+        });
+    });
+
+    describe('#findLowestPrecedence', () => {
+        it('should find the lowest precedence operator', () => {
+            let tree = new ExpressionTree('');
+            assert.strictEqual(tree.findLowestPrecedence('4*5+1-4/12').lowestPrecedenceOperator, '-');
+            assert.strictEqual(tree.findLowestPrecedence('4*5+1').lowestPrecedenceOperator, '+');
+            assert.strictEqual(tree.findLowestPrecedence('4*5').lowestPrecedenceOperator, '*');
+            assert.strictEqual(tree.findLowestPrecedence('4').lowestPrecedenceOperator, null);
+        });
+    });
+    
+    describe('#buildNodePointers', () => {
+        it('should reconfigure a binary node\'s null pointers', () => {
+            let tree = new ExpressionTree('');
+            let node1 = tree.buildNodePointers(new BinaryNode(null, null, '+'), ['3', '+', '5']);
+            assert.strictEqual(node1.data, '+');
+            assert.strictEqual(node1.left.data, '3');
+            assert.strictEqual(node1.right.data, '5');
+            let node2 = tree.buildNodePointers(new BinaryNode(null, null, '*'), ['9', '^', '8', '/', '3', '*', '5']);
+            assert.strictEqual(node2.data, '*');
+            assert.strictEqual(node2.left.data, '/');
+            assert.strictEqual(node2.right.data, '5');
+            assert.strictEqual(node2.left.left.data, '^');
+            assert.strictEqual(node2.left.right.data, '3');
+            assert.strictEqual(node2.left.left.left.data, '9');
+            assert.strictEqual(node2.left.left.right.data, '8');
+        });
+    });
+    
+    describe('#buildNode', () => {
+        it('should build a node with data equal to \'5\' and null ptrs', () => {
+            let tree = new ExpressionTree('');
+            let node = tree.buildNode(['5']);
+            assert.strictEqual(node.data, '5');
+            assert.strictEqual(node.left, null);
+            assert.strictEqual(node.right, null);
         });
     });
 });
