87cb47a54da5d5d7a16cc9aa2fd6dd6f6814f549
Zebediah Perkins <zebmperkins@gmail.com>
2020-07-11 19:16:42 -0400

    version 2.2.0: add util.js to exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     "BinaryNode": require('./src/BinaryNode'),
     "ExpressionTree": require('./src/ExpressionTree'),
-    "BinaryTree": require('./src/BinaryTree')
+    "BinaryTree": require('./src/BinaryTree'),
+    "util": require('./src/util')
 }
